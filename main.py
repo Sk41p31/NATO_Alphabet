efe9bb2677cac8ed12a71c6801dc854a9753e323
Sk41p31 <mibu989@gmail.com>
2023-03-29 18:45:02 +0200

    Added KeyError handling - in case of incorrect input.

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ nato_letters = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet = {row["letter"]:row["code"] for (index, row) in nato_letters.iterrows()}
 
-user_input = input("Input the word you want to convert to NATO alphabet: ")
-user_input = user_input.upper()
+correct_input = False
+while not correct_input:
+    user_input = input("Input the word you want to convert to NATO alphabet: ")
+    user_input = user_input.upper()
+    try:
+        output = [nato_alphabet[letter] for letter in user_input]
+    except KeyError:
+        print("Use only letters in English the alphabet!\n")
+    else:
+        correct_input = True
 
-output = [nato_alphabet[letter] for letter in user_input]
+
 print(output)
